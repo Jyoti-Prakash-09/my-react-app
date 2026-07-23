655e00fb1ac23a5ca6ae1fb278edf07be6c1f978
Ubuntu <ubuntu@ip-172-31-43-74.ap-south-1.compute.internal>
2026-07-23 17:24:09 +0000

    intial

---
+++ run_push.sh
@@ -0,0 +1,3 @@
+git remote remove origin 2>/dev/null
+git remote add origin https://github.com
+git push -u origin main
---
+++ upload.sh
@@ -0,0 +1,1 @@
+git remote remove origin; git remote add origin https://github.com/Jyoti-Prakash-09/my-react-app.git; git push -u origin main
